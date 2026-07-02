3e77e9a2aa2aff69798fb0ca95459fc955b3bfd7
HafizMMoaz <hafizmoazkhalid@gmail.com>
2026-07-02 11:54:21 +0500

    Set package description

--- src/Database/Seeders/MarketplaceSettingSeeder.php
+++ src/Database/Seeders/MarketplaceSettingSeeder.php
@@ -18,7 +18,7 @@ class MarketplaceSettingSeeder extends Seeder
             $files = File::files($marketplaceDir);
             foreach ($files as $file) {
                 if (in_array($file->getExtension(), ['png', 'jpg', 'jpeg', 'gif', 'webp'])) {
-                    $screenshots[] = '/packages/workdo/Taskly/src/marketplace/' . $file->getFilename();
+                    $screenshots[] = '/packages/local/Taskly/src/marketplace/' . $file->getFilename();
                 }
             }
         }
@@ -33,7 +33,7 @@ class MarketplaceSettingSeeder extends Seeder
                 'sections' => [
                     'hero' => [
                         'variant' => 'hero1',
-                        'title' => 'Taskly Module for ERPGo SaaS',
+                        'title' => 'Taskly Module for Zerp',
                         'subtitle' => 'Streamline your taskly workflow with comprehensive tools and automated management.',
                         'primary_button_text' => 'Install Taskly Module',
                         'primary_button_link' => '#install',
@@ -55,19 +55,19 @@ class MarketplaceSettingSeeder extends Seeder
                                 'title' => 'Feature 1',
                                 'description' => 'Description of first key feature for taskly management.',
                                 'keyPoints' => ['Point 1', 'Point 2', 'Point 3', 'Point 4'],
-                                'screenshot' => '/packages/workdo/Taskly/src/marketplace/image1.png'
+                                'screenshot' => '/packages/local/Taskly/src/marketplace/image1.png'
                             ],
                             [
                                 'title' => 'Feature 2',
                                 'description' => 'Description of second key feature for taskly organization.',
                                 'keyPoints' => ['Point 1', 'Point 2', 'Point 3', 'Point 4'],
-                                'screenshot' => '/packages/workdo/Taskly/src/marketplace/image2.png'
+                                'screenshot' => '/packages/local/Taskly/src/marketplace/image2.png'
                             ],
                             [
                                 'title' => 'Feature 3',
                                 'description' => 'Description of third key feature for taskly tracking.',
                                 'keyPoints' => ['Point 1', 'Point 2', 'Point 3', 'Point 4'],
-                                'screenshot' => '/packages/workdo/Taskly/src/marketplace/image3.png'
+                                'screenshot' => '/packages/local/Taskly/src/marketplace/image3.png'
                             ]
                         ]
                     ],
